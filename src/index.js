da139a760b7027a43de7707235f7b76909a3cc93
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-04-16 12:08:17 -0400

    Life Cycle Demo
    Added Life Cycle Demo class which is an example of a components life cycle

--- src/index.js
+++ src/index.js
@@ -11,9 +11,42 @@ class ErrorCatcher extends React.Component {
     this.setState({ error: info.componentStack });
   }
   render() {
+    if (this.state.error) {
+      return <div>An Error Occured {this.state.error}</div>;
+    }
+    return this.props.children;
+  }
+}
+
+class LifeCycleDemo extends React.Component {
+  state = { counter: 0 };
+
+  constructor(props) {
+    super(props);
+    console.log("[constructor");
+    console.log("State already set", this.state);
+  }
+
+  componentDidMount() {
+    console.log("[componentDidMount]", "Mounted");
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("[getDerivedStateFromProps");
+    console.log("   Next props:", nextProps);
+    console.log("   Prev state:", prevState);
+    return null;
+  }
+
+  shouldComponentUpdate(nextProps, prevProps) {
+    console.log("[shouldComponentUpdate", "Deciding to update");
+    return true;
+  }
+
+  render() {
     return <div />;
   }
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<ErrorCatcher />, rootElement);
+ReactDOM.render(<LifeCycleDemo />, rootElement);
